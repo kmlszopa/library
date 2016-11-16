1dbf5500291962964e83db44a7e00cdd335616d2
Daniel Blacha <daniel.blacha@gmail.com>
2016-11-16 14:02:26 +0100

    added Book do package domain

--- src/pl/akademiakodu/library/Main.java
+++ src/pl/akademiakodu/library/Main.java
@@ -3,6 +3,6 @@ package pl.akademiakodu.library;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("ss");
     }
 }
---
+++ src/pl/akademiakodu/library/domain/Book.java
@@ -0,0 +1,57 @@
+package pl.akademiakodu.library.domain;
+
+/**
+ * Created by Daniel_B on 16.11.2016.
+ */
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int pages;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(int id, String title, String author, int pages) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+}
