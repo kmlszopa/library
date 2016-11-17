fa29699552d14e98e6956989500ab715756e7824
Kamil Szopaan <kmlszopa@gmail.com>
2016-11-17 11:10:54 +0100

    semi final

--- src/main/java/pl/akademiakodu/library/dao/BookDaoSqlite.java
+++ src/main/java/pl/akademiakodu/library/dao/BookDaoSqlite.java
@@ -2,6 +2,7 @@ package pl.akademiakodu.library.dao;
 
 import pl.akademiakodu.library.domain.Book;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -25,44 +26,58 @@ public class BookDaoSqlite implements BookDao {
         createTable();
     }
 
-    private void createTable(){
-        String sql="CREATE TABLE IF NOT EXISTS Books ("
-                +"id INTEGER PRIMARY KEY AUTOINCREMENT, "+
+    private void createTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Books ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "title TEXT, "
-                +"author TEXT, "
-                +"pages INTEGER "
-                +")";
+                + "author TEXT, "
+                + "pages INTEGER "
+                + ")";
 
-        try{
-            Statement statement=connection.createStatement();
+        try {
+            Statement statement = connection.createStatement();
             statement.executeUpdate(sql);
 
-        }catch (SQLException e){
-            System.out.println("Nie udało sie wykonać SQL "+e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Nie udało sie wykonać SQL " + e.getMessage());
         }
     }
 
     public static void main(String[] args) {
         BookDaoSqlite bookDaoSqlite = new BookDaoSqlite();
-        bookDaoSqlite.addBook( new Book("Pozytywne myślenie","Brayan Stacey",359));
+        bookDaoSqlite.addBook(new Book("Pozytywne myślenie", "Brayan Stacey", 359));
 
     }
 
     public void addBook(Book book) {
-        String sql = "INSERT INTO Books (title,author,pages)"
-                +"VALUES ('"+book.getTitle()+"','"+book.getAuthor()+"',"+book.getPages()+")";
-        try{
-            Statement statement=connection.createStatement();
+        String simpleClassName = book.getClass().getSimpleName();
+        Field[] fields = book.getClass().getDeclaredFields();
+        StringBuilder attributeString = new StringBuilder("(");
+        for (Field field:fields){
+            if(!field.getName().equals("id")) {
+                attributeString.append(field.getName());
+                attributeString.append(",");
+            }
+
+
+        }
+        attributeString.deleteCharAt(attributeString.length()-1);
+        attributeString.append(")");
+        System.out.println(attributeString);
+        String sql = "INSERT INTO " + simpleClassName + "s "+attributeString
+                + "VALUES ('" + book.getTitle() + "','" + book.getAuthor() + "'," + book.getPages() + ")";
+        try {
+            Statement statement = connection.createStatement();
             statement.execute(sql);
 
-        }catch (SQLException e){
-            System.out.println("Nie udało sie wykonać SQL "+e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Nie udało sie wykonać SQL " + e.getMessage());
         }
     }
 
 
     public void removeBook(Book book) {
-       }
+    }
 
 
     public List<Book> getAllBooks() {
